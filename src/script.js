16b73e195a833fe7d752b4ebb2e2f550a0c147ae
iamsadeem <iamsadeem@proton.me>
2024-04-07 15:05:10 +0300

    validation through modal box

--- src/script.js
+++ src/script.js
@@ -6,6 +6,41 @@ const female = document.getElementById("female");
 const male = document.getElementById("male");
 const submitButton = document.getElementById("submit-btn");
 let resultArea = document.querySelector(".comment");
+let modalContent = document.querySelector(".modal-content");
+let modalText = document.querySelector("#modal-text");
+const modal = document.getElementById("modal");
+const closeButton = document.getElementsByClassName("close-btn")[0];
+
+// Event listeners for input fields to handle validation
+age.addEventListener('input', validateInput);
+height.addEventListener('input', validateInput);
+weight.addEventListener('input', validateInput);
+
+// Function to handle input validation
+function validateInput() {
+    const input = this;
+    const inputName = input.getAttribute('id');
+    const inputValue = parseFloat(input.value);
+    if (inputValue <= 0 || isNaN(inputValue)) {
+        showModal(`${capitalizeFirstLetter(inputName)} cannot be negative or 0.`);
+        input.value = "";
+    } else if (input.value.length >= 3) {
+        showModal(`${capitalizeFirstLetter(inputName)} cannot be more than 3 digits.`);
+        input.value = input.value.slice(0, 3);
+        input.blur();
+    }
+}
+// Function to show the modal with specified message
+function showModal(message) {
+    modal.style.display = "block";
+    modalText.innerHTML = message;
+}
+
+// Function to capitalize the first letter of a string
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 
 // Event listener for clicks on submit button to call calculate function
 submitButton.addEventListener('click', () => {
@@ -19,10 +54,24 @@ document.addEventListener('keyup', (event) => {
     }
 });
 
+// Close the modal when the user clicks on close button 
+closeButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close the modal when the user clicks anywhere outside of the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 // Function to ensure all inputs are filled then calculate BMI and display the result in HTML 
 function calculate() {
-    if (age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)) {
+    if (age.value == '' || height.value == '' || weight.value == '' || (male.checked == false && female.checked == false)) {
         console.log("All fields are required!");
+        modal.style.display = "block";
+        modalText.innerHTML = `All fields are required!`;
 
     } else {
         var person = [age.value, height.value, weight.value];
@@ -50,7 +99,7 @@ function calculate() {
         person.push(bmi.toFixed(2));
         person.push(result);
         console.log(person);
-        document.querySelector(".comment").innerHTML = `You are <span id="comment">${result}!</span>`;
         document.querySelector("#result").innerHTML = bmi.toFixed(2);
+        resultArea.innerHTML = `You are <span id="comment">${result}!</span>`;
     }
 }
